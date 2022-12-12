25d534570382e4659c0b402c69fbb8345a994fd6
surbhi <84731799+surbhigulhana@users.noreply.github.com>
2022-12-12 12:14:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,115 @@
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const path =require("path")
-require('./Model/config')
+const path = require("path");
+require("./Model/config");
 app.use(cors());
 app.use(express.json());
-const signup =require("./Router/SignupApi")
-const AllProduct =require("./Router/AllproductApi");
-const product =require("./Router/ProdcutApi");
-const category =require("./Router/CAtegory")
-const order =require("./Router/Order")
-const inquiry =require("./Router/Inquiry");
-const Ticket =require("./Router/Ticket")
-const attribute =require("./Router/Attribute")
-const coupon =require('./Router/Coupon')
-const Payment =require("./Model/Payment")
-const Type =require("./Model/attributeType")
+const signup = require("./Router/SignupApi");
+const AllProduct = require("./Router/AllproductApi");
+const product = require("./Router/ProdcutApi");
+const category = require("./Router/CAtegory");
+const order = require("./Router/Order");
+const inquiry = require("./Router/Inquiry");
+const Ticket = require("./Router/Ticket");
+const attribute = require("./Router/Attribute");
+const coupon = require("./Router/Coupon");
+const Payment = require("./Model/Payment");
+const Type = require("./Model/attributeType");
 // const uploadImg =require("./Router/Imag")
-const StoreName =require('./Model/StoreName')
+const Cart = require("./Model/Cart");
+const StoreName = require("./Model/StoreName");
 
 const BannerImg = require("./Model/BannerImg");
 
-app.use("/",signup,AllProduct,coupon,Ticket,category,product,inquiry,attribute,order);
+app.use(
+  "/",
+  signup,
+  AllProduct,
+  coupon,
+  Ticket,
+  category,
+  product,
+  inquiry,
+  attribute,
+  order
+);
 
-  app.use(express.static(path.join(__dirname,"public")));
-  const multer =require("multer");
+app.use(express.static(path.join(__dirname, "public")));
+const multer = require("multer");
 const storage = multer.diskStorage({
- destination:'./public/uploads',
- filename:(req,file,cb)=>{
-  return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
- }
-  })
-  const upload = multer({
-  storage:storage
-  })
-app.use("/filename",express.static("./public/uploads"))
+  destination: "./public/uploads",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+app.use("/filename", express.static("./public/uploads"));
 //-------------------------------payemnt---------------------------------------------
-  app.post(
-    "/api/payment",
-    upload.single("filename"),
-    async function (req, res) {
-      const { Pname, UserEmail, PurchaseDate, Mode, TotalAmt,Point, Status,Tpoint} = req.body;
-    
-      try {
-        const result1 = new Payment({
-          Pname: Pname,
-          UserEmail: UserEmail,
-          Mode: Mode,
-          PurchaseDate: PurchaseDate,
-          TotalAmt: TotalAmt,
-          Point:TotalAmt/10,
-          Status: Status,
-        });
-       
-        const data = await result1.save();
-      
-        console.log(data);
-        res.status(200).json({ success: true, data: result1});
-      
-      } 
-      
-      catch (err) {
-        console.log(err);
-        res.status(500).json({ success: false });
-      }
-      
-    }
-  );
-  app.get("/payment", async (req, resp) => {
-    let result = await Payment.find();
-    resp.send(result);
-  });
-  
-  app.get("/payment/:Pname", async (req, resp) => {
-    try {
-      const id = req.params.Pname;
-  
-      let result = await Payment.find({ Pname: id },{'Pname':1,'Point':1});
-      console.log(result);
-      resp.status(200).send({result});
-    
-    } catch (err) {
-      console.log("err : ", err);
-      resp.status(400).json(err);
-    }
-  });
-  
-  app.delete("/payment/:_id", async (req, resp) => {
-    let result = await Payment.deleteOne(req.params);
-    resp.send(result);
-  });
-  app.put("/payment/:_id", async (req, resp) => {
-    let result = await Payment.updateOne(req.params, { $set: req.body });
-    console.log(req.params);
-    resp.send(result);
-  });
+app.post("/api/payment", upload.single("filename"), async function (req, res) {
+  const {
+    Pname,
+    UserEmail,
+    PurchaseDate,
+    Mode,
+    TotalAmt,
+    Point,
+    Status,
+    Tpoint,
+  } = req.body;
+
+  try {
+    const result1 = new Payment({
+      Pname: Pname,
+      UserEmail: UserEmail,
+      Mode: Mode,
+      PurchaseDate: PurchaseDate,
+      TotalAmt: TotalAmt,
+      Point: TotalAmt / 10,
+      Status: Status,
+    });
+
+    const data = await result1.save();
+
+    console.log(data);
+    res.status(200).json({ success: true, data: result1 });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+});
+app.get("/payment", async (req, resp) => {
+  let result = await Payment.find();
+  resp.send(result);
+});
+
+app.get("/payment/:Pname", async (req, resp) => {
+  try {
+    const id = req.params.Pname;
+
+    let result = await Payment.find({ Pname: id }, { Pname: 1, Point: 1 });
+    console.log(result);
+    resp.status(200).send({ result });
+  } catch (err) {
+    console.log("err : ", err);
+    resp.status(400).json(err);
+  }
+});
+
+app.delete("/payment/:_id", async (req, resp) => {
+  let result = await Payment.deleteOne(req.params);
+  resp.send(result);
+});
+app.put("/payment/:_id", async (req, resp) => {
+  let result = await Payment.updateOne(req.params, { $set: req.body });
+  console.log(req.params);
+  resp.send(result);
+});
 
 //----------------------------------------------------------------------------
 //----------------------------------------type---------------------------------
@@ -138,7 +152,6 @@ app.delete("/Type/:_id", async (req, resp) => {
   resp.send(result);
 });
 
-
 app.get("/display/:id", async (req, res) => [
   Type.find({ AttributeName: req.params.id })
     .populate("AttributeType")
@@ -148,16 +161,46 @@ app.get("/display/:id", async (req, res) => [
     }),
 ]);
 
-//  ----------------------------------------------- // 
+//  ----------------------------------------------- //
+app.post("/BannerImg", upload.single("filename"), async function (req, res) {
+  const { myfilename } = req.body;
+  try {
+    const result1 = new BannerImg({
+      filename: `http://3.114.92.202:4003/filename/${req.file.filename}`,
+    });
+    const data = await result1.save();
+    console.log(data);
+    res.status(200).json({ success: true, data: result1 });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+});
+app.get("/BannerImg", async (req, resp) => {
+  let result = await BannerImg.find();
+  resp.send(result);
+});
+app.post("/BannerImg/:_id", upload.single("filename"), async (req, resp) => {
+  const { myfilename } = req.body;
+  let result = await BannerImg.updateOne(req.params, {
+    $set: {
+      filename: `http://3.114.92.202:4003/filename/${req.file.filename}`,
+    },
+  });
+  console.log(req.params);
+  resp.send(result);
+});
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// store
 app.post(
-  "/BannerImg",
+  "/api/StoreName",
   upload.single("filename"),
   async function (req, res) {
-    const { myfilename } = req.body;
+    const { Name } = req.body;
+
     try {
-      const result1 = new BannerImg({
-        filename:`http://3.114.92.202:4003/filename/${req.file.filename}`
-       
+      const result1 = new StoreName({
+        Name: Name,
       });
       const data = await result1.save();
       console.log(data);
@@ -168,61 +211,47 @@ app.post(
     }
   }
 );
-app.get("/BannerImg", async (req, resp) => {
-  let result = await BannerImg.find();
-  resp.send(result);
-});
-app.post(
-  "/BannerImg/:_id",
-  upload.single("filename"),
-  async (req, resp) => {
-    const {
-      
-      myfilename,} = req.body;
-    let result = await BannerImg.updateOne(req.params, {
-      $set: {
-        filename:`http://3.114.92.202:4003/filename/${req.file.filename}` },
-    });
-    console.log(req.params);
-    resp.send(result);
-  }
-);
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// store
-app.post( "/api/StoreName",
-  upload.single("filename"),
-  async function (req, res) {
-    const { Name} = req.body;
-  
-    try {
-      const result1 = new StoreName({
-   Name:Name 
-      });
-      const data = await result1.save();
-      console.log(data);
-      res.status(200).json({ success: true, data: result1});
-    
-    } 
-    
-    catch (err) {
-      console.log(err);
-      res.status(500).json({ success: false });
-    }
-    
-  }
-);
 app.get("/StoreName", async (req, resp) => {
   let result = await StoreName.find();
   resp.send(result);
 });
 
-
 app.delete("/StoreName/:_id", async (req, resp) => {
   let result = await StoreName.deleteOne(req.params);
   resp.send(result);
 });
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// Add to Cart
+app.post("/api/AddCart", upload.single("filename"), async function (req, res) {
+  const { Pname, Price, Qty, email } = req.body;
+  try {
+    const result1 = new Cart({
+      Pname: Pname,
+      Price: Price,
+      Qty: Qty,
+      email: email,
+      TotalPrice: Price * Qty,
+      filename: `http://3.114.92.202:4003/filename/${req.file.filename}`,
+    });
+    const data = await result1.save();
+    console.log(data);
+    res.status(200).json({ success: true, data: result1 });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+});
+app.get("/Cart/:email", async (req, resp) => {
+  try {
+    const id = req.params.email;
 
-
-app.listen(4003)
-console.log ('server run on 4003')
-
+    let result = await Cart.find({ email: id });
+    console.log(result);
+    resp.status(200).send({ result });
+  } catch (err) {
+    console.log("err : ", err);
+    resp.status(400).json(err);
+  }
+});
+app.listen(4003);
+console.log("server run on 4003");
