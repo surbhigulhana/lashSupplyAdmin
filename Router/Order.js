cb8e20c33bba0413a3cd8d9bd4e67074212a1441
surbhi <84731799+surbhigulhana@users.noreply.github.com>
2023-02-16 17:31:59 +0530

    Update Order.js

--- Router/Order.js
+++ Router/Order.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Order =require("../Model/Order")
+const ProductName= require("../Model/AllProduct")
 var upload = require("../config/multer");
 router.post(
     "/api/order",
@@ -27,6 +28,33 @@ router.post(
       }
     }
   );
+  router.post(
+    "/api/Myorder/:Name", upload.single('imgCollection', 1),
+    async function (req, res) {
+      const id = req.params.Name;
+  let result = await ProductName.find({ Name: id },{imgCollection:1});
+  console.log(result[0].imgCollection[0]);
+      const {orderNumber,UserName, Quantity,AttributeType1,AttributeType2,Status} = req.body;
+      try {
+        const result1 = new Order({
+     orderNumber:orderNumber,
+      Name:id,
+      UserName:UserName,
+    Quantity:Quantity,
+    AttributeType1:AttributeType1,
+    AttributeType2:AttributeType2,
+    imgCollection:result[0].imgCollection[0],
+    Status:Status,
+        });
+        const data = await result1.save();
+        console.log(data);
+        res.status(200).json({ success: true, data: result1 });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false });
+      }
+    }
+  );
   router.get("/order", async (req, resp) => {
     let result = await Order.find();
     resp.send(result);
@@ -36,13 +64,14 @@ router.post(
     const id=req.params.UserName;
     console.log(id)
    let result = await Order.find({UserName:id});
-    resp.status(200).send(result);
+    resp.status(200).send({result});
     }
     catch(err){
         console.log("err : ",err)
         resp.status(400).json(err);
     }
   })
+ 
   router.get("/pendingOrder", async (req, resp) => {
     let result = await Order.find({Status:"Pending"});
     resp.send(result);
