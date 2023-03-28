e0cf43cd441c338a4d09673db8ba16929eeb1149
surbhi <84731799+surbhigulhana@users.noreply.github.com>
2023-03-28 14:31:31 +0530

    Update Order.js

--- Model/Order.js
+++ Model/Order.js
@@ -1,102 +1,37 @@
-// var mongoose = require("mongoose");
-
-// const schema3 = new mongoose.Schema({
-//   uid:{type:'ObjectId'},
- 
-//   Pname: {
-//     type: String
-
-//   },
-//   UserName:{
-//     type:String,
- 
-//   },
-//   Phone:{
-//     type:String
-//   },
-//   PurchaseDate:{
-//     type:String
-//   },
-//   TotalAmt:{
-//     type:String
-//   },
-//   Status:{
-//     type:String,
-//     default:"Pending"
-//   },
-//   ShipAdd:{
-//     type:String
-//   },
-//   BillingAdd:{
-//     type:String
-//   },
-  
-
-  
-  
- 
-// });
-
-// const Order = new mongoose.model("purchaseOrder", schema3);
-
-// module.exports = Order;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var mongoose = require("mongoose");
 
 const schema3 = new mongoose.Schema({
-  uid:{type:'ObjectId'},
+  uid: { type: "ObjectId" },
 
- orderNumber:{
-  type:String,
- },
-  Name: {
-    type: String
-
+  Pname: {
+    type: String,
   },
-  UserName:{
-    type:String,
- 
+  UserName: {
+    type: String,
   },
-  Quantity:{
-    type:Number
+  Quantity: {
+    type: Number,
   },
-  imgCollection: {
-    type: String ,
-  }
-  ,
+  PurchaseDate: {
+    type: String,
+  },
   TotalAmt: {
-    type: Number
-
+    type: String,
   },
-
-AttributeType1:{
-  type:Array,
-},
-AttributeType2:{
-  type:Array
-} , Status:{
-  type:String,
- default:"Pending"
-},
- 
+  Status: {
+    type: String,
+    default: "Pending",
+  },
+  ShipAdd: {
+    type: String,
+  },
+  BillingAdd: {
+    type: String,
+  },
 });
 
 const Order = new mongoose.model("purchaseOrder", schema3);
 
 module.exports = Order;
+
+
