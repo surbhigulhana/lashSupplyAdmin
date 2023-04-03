997bc17905fa22eb4223e5930fb5afd142e86850
surbhi <84731799+surbhigulhana@users.noreply.github.com>
2023-04-03 15:46:52 +0530

    Update Order.js

--- Model/Order.js
+++ Model/Order.js
@@ -28,10 +28,68 @@ const schema3 = new mongoose.Schema({
   BillingAdd: {
     type: String,
   },
+  Phone:{
+    type:String
+  },
+  AttributeType1:{
+    type:String,
+  },
+  AttributeType2:{
+    type:String
+  },
+  Landmark:{
+    type:String
+  },
+  PostalCode:{
+    type:String
+  }
 });
 
 const Order = new mongoose.model("purchaseOrder", schema3);
 
 module.exports = Order;
 
+// var mongoose = require("mongoose");
 
+// const schema3 = new mongoose.Schema({
+//   uid:{type:'ObjectId'},
+
+//  orderNumber:{
+//   type:String,
+//  },
+//   Name: {
+//     type: String
+
+//   },
+//   UserName:{
+//     type:String,
+
+//   },
+//   Quantity:{
+//     type:Number
+//   },
+//   imgCollection: {
+//     type: String ,
+//   }
+//   ,
+//   TotalAmt: {
+//     type: Number
+
+//   },
+
+// AttributeType1:{
+//   type:Array,
+// },
+// AttributeType2:{
+//   type:Array
+// } ,
+//  Status:{
+//   type:String,
+//  default:"Pending"
+// },
+
+// });
+
+// const Order = new mongoose.model("purchaseOrder", schema3);
+
+// module.exports = Order;
